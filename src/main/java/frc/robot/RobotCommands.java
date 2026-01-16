411e7ae9646559b70f9e6d4a4311fc6717d89e96
liujip0 <liujip2020@gmail.com>
2026-01-16 18:40:02 -0500

    create RobotCommands

---
+++ src/main/java/frc/robot/RobotCommands.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import frc.robot.subsystems.drive.Drive;
+
+public class RobotCommands {
+  private final Drive drive;
+
+  public RobotCommands(Drive drive) {
+    this.drive = drive;
+  }
+
+  
+}
