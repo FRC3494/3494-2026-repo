1e10f07506a35520f3f84892b7439e97de6f6317
liujip0 <liujip2020@gmail.com>
2026-02-14 21:51:17 -0500

    new drive motor ks and kv

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -179,8 +179,8 @@ public final class Constants {
     // Drive PID configuration
     public static final double driveKp = 0.0;
     public static final double driveKd = 0.0;
-    public static final double driveKs = 0.0;
-    public static final double driveKv = 0.1;
+    public static final double driveKs = 0.16482690;
+    public static final double driveKv = 0.10937844;
     public static final double driveSimP = 0.05;
     public static final double driveSimD = 0.0;
     public static final double driveSimKs = 0.0;
