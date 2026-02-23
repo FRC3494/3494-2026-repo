fe67153943fe5af744ff0ac0c8e129e1a1cc34e6
liujip0 <liujip2020@gmail.com>
2026-02-22 20:05:34 -0500

    new drive module zeros

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -187,10 +187,10 @@ public final class Constants {
         };
 
     // Zeroed rotation values for each module, see setup instructions
-    public static final Rotation2d frontLeftZeroRotation = Rotation2d.fromRadians(2.397);
-    public static final Rotation2d frontRightZeroRotation = Rotation2d.fromRadians(6.215);
-    public static final Rotation2d backLeftZeroRotation = Rotation2d.fromRadians(3.552);
-    public static final Rotation2d backRightZeroRotation = Rotation2d.fromRadians(5.134);
+    public static final Rotation2d frontLeftZeroRotation = Rotation2d.fromRadians(5.515);
+    public static final Rotation2d frontRightZeroRotation = Rotation2d.fromRadians(3.068);
+    public static final Rotation2d backLeftZeroRotation = Rotation2d.fromRadians(0.417);
+    public static final Rotation2d backRightZeroRotation = Rotation2d.fromRadians(1.974);
 
     // Drive motor configuration
     public static final boolean driveInverted = true;
