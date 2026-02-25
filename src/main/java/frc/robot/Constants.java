5353d7d2a9dff7b014ea0f3c107e3c3c826b3b71
liujip0 <liujip2020@gmail.com>
2026-02-24 21:32:19 -0500

    new pigeon yaw trim

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -269,7 +269,7 @@ public final class Constants {
     // Pigeon config
     public static final double pigeonGyroTrimXDegPerRot = 0.0;
     public static final double pigeonGyroTrimYDegPerRot = 0.0;
-    public static final double pigeonGyroTrimZDegPerRot = 0.0;
+    public static final double pigeonGyroTrimZDegPerRot = -1.88;
 
     public static final Angle pigeonMountPoseYaw = Degrees.of(-0.032393235713243484);
     public static final Angle pigeonMountPosePitch = Degrees.of(-0.30914029479026794);
