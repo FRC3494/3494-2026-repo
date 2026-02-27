05538ba824a6a4b1276fe37c15890f7d4d71e2ca
liujip0 <liujip2020@gmail.com>
2026-02-26 23:00:17 -0500

    import units for 578fa733b62a7c0d948de2a4002311a96faa74f0

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -31,6 +31,7 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
