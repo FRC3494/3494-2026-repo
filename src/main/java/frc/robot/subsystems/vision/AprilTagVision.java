f058c85615b82fd5acdb3d84ccef0fccf334e2e3
liujip0 <liujip2020@gmail.com>
2026-03-02 22:31:40 -0500

    make LL stddevs tunable

--- src/main/java/frc/robot/subsystems/vision/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/vision/AprilTagVision.java
@@ -3,14 +3,21 @@ package frc.robot.subsystems.vision;
 import static edu.wpi.first.units.Units.*;
 import static frc.robot.Constants.VisionConstants.*;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotMap;
 import frc.robot.subsystems.drive.Drive;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class AprilTagVision extends SubsystemBase {
   private AprilTagCamera[] cameras = new AprilTagCamera[RobotMap.Vision.aprilTagLimelights.length];
 
   private Drive drive;
+
+  private final LoggedNetworkNumber distanceStdDev =
+      new LoggedNetworkNumber("Tunable/Vision/DistanceStdDev", maxDistanceStdDev);
+  private final LoggedNetworkNumber angleStdDev =
+      new LoggedNetworkNumber("Tunable/Vision/AngleStdDev", maxAngleStdDev);
 
   public AprilTagVision(Drive drive) {
     this.drive = drive;
@@ -34,7 +41,9 @@ public class AprilTagVision extends SubsystemBase {
 
       if (camera.isValidMeasurement()) {
         drive.addVisionMeasurement(
-            camera.getPose(), camera.getPoseTimestamp(), camera.getStdDevs());
+            camera.getPose(),
+            camera.getPoseTimestamp(),
+            VecBuilder.fill(distanceStdDev.get(), distanceStdDev.get(), angleStdDev.get()));
       }
     }
   }
