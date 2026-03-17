47c47a540c4a1d41a2c242beb1343692057b79d8
Skyler Fortune <skylerforutne84@gmail.com>
2026-03-17 17:59:16 -0400

    Updated current limit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -213,7 +213,7 @@ public final class Constants {
 
     // Drive motor configuration
     public static final boolean[] driveInverted = new boolean[] {true, true, true, true};
-    public static final int driveMotorCurrentLimit = 60;
+    public static final int driveMotorCurrentLimit = 50;
     public static final double wheelRadiusMeters =
         Units.inchesToMeters(
             2.29793665); // When using linear characterization: actual linear distance / wheel delta
