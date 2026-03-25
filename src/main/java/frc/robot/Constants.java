5efc3a70f514da48ef4ae52075fe861c8d8c61d5
liujip0 <liujip2020@gmail.com>
2026-03-25 02:25:24 -0400

    new max speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -189,7 +189,7 @@ public final class Constants {
        - Divide max rotation speed when driving by max rotation speed while stationary
     */
 
-    public static final double maxSpeedMetersPerSec = 4.56; // 14.961 ft/s
+    public static final double maxSpeedMetersPerSec = 4.62906; // 15.187 ft/s
     // * Max rotation speed (Rad/Sec) while moving / Max rotation speed while stationary
     public static final double maxAngularSpeedFactor = (321.5229038 / 630.028839);
     public static final double odometryFrequency = 100.0; // Hz
