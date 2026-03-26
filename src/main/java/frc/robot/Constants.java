a17c2d98a483150e204225ccdc953c1358ca4a97
liujip0 <liujip2020@gmail.com>
2026-03-25 22:44:45 -0400

    new LERP points

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -423,25 +423,43 @@ public final class Constants {
 
     public static final LinearInterpolationDataPoint[] linearInterpolationDataPoints =
         new LinearInterpolationDataPoint[] {
-          // TODO: need NZ shot time of flight
+          // TODO: need time of flight
+          // * Tuned front towards hub
           new LinearInterpolationDataPoint(
-              Meters.of(9.170), Rotation2d.fromDegrees(45.0), RPM.of(4500.0), Seconds.of(0.0)),
+              Meters.of(1.111), Rotation2d.fromDegrees(24.2238027), RPM.of(2700), Seconds.of(0.0)),
           new LinearInterpolationDataPoint(
-              Meters.of(5.530), Rotation2d.fromDegrees(45.0), RPM.of(3750.0), Seconds.of(1.26)),
+              Meters.of(1.868), Rotation2d.fromDegrees(27), RPM.of(2900), Seconds.of(0.0)),
           new LinearInterpolationDataPoint(
-              Meters.of(3.315), Rotation2d.fromDegrees(35.0), RPM.of(3200.0), Seconds.of(1.25)),
+              Meters.of(2.054), Rotation2d.fromDegrees(29), RPM.of(2950), Seconds.of(0.0)),
           new LinearInterpolationDataPoint(
-              Meters.of(2.157), Rotation2d.fromDegrees(30.0), RPM.of(3000.0), Seconds.of(1.2)),
+              Meters.of(2.235), Rotation2d.fromDegrees(30), RPM.of(2975), Seconds.of(0.0)),
           new LinearInterpolationDataPoint(
-              Meters.of(1.492),
-              Rotation2d.fromDegrees(24.2238027),
-              RPM.of(2800.0),
-              Seconds.of(1.16)),
+              Meters.of(2.633), Rotation2d.fromDegrees(32), RPM.of(3050), Seconds.of(0.0)),
           new LinearInterpolationDataPoint(
-              Meters.of(1.053),
-              Rotation2d.fromDegrees(24.2238027),
-              RPM.of(2800.0),
-              Seconds.of(1.16))
+              Meters.of(2.852), Rotation2d.fromDegrees(33), RPM.of(3100), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(3.125), Rotation2d.fromDegrees(34), RPM.of(3150), Seconds.of(0.0)),
+          // * Tuned side towards hub
+          new LinearInterpolationDataPoint(
+              Meters.of(3.268), Rotation2d.fromDegrees(33), RPM.of(3100), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(3.546), Rotation2d.fromDegrees(34), RPM.of(3175), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(3.66), Rotation2d.fromDegrees(33), RPM.of(3300), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(3.695), Rotation2d.fromDegrees(32), RPM.of(3425), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(3.826), Rotation2d.fromDegrees(35), RPM.of(3275), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(4.045), Rotation2d.fromDegrees(35), RPM.of(3350), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(4.23), Rotation2d.fromDegrees(35), RPM.of(3440), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(4.64), Rotation2d.fromDegrees(36), RPM.of(3550), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(5.24), Rotation2d.fromDegrees(38), RPM.of(3675), Seconds.of(0.0)),
+          new LinearInterpolationDataPoint(
+              Meters.of(9.33), Rotation2d.fromDegrees(45), RPM.of(4500), Seconds.of(0.0))
         };
 
     public static record LinearInterpolationDataPoint(
