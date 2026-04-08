2eaaa951b9bb71ecb96b60befb51f8c142098911
liujip0 <liujip2020@gmail.com>
2026-04-08 17:24:47 -0400

    add left and right PreTrench poses to AutoAlignConstants

--- src/main/java/frc/robot/util/choreo/ChoreoVars.java
+++ src/main/java/frc/robot/util/choreo/ChoreoVars.java
@@ -31,11 +31,12 @@ public final class ChoreoVars {
         public static final Pose2d ClimbSetupDepot = new Pose2d(1.053, 4.9, Rotation2d.fromRadians(1.571));
         public static final Pose2d ClimbSetupOutpost = new Pose2d(1.053, 2.65, Rotation2d.fromRadians(-1.571));
         public static final Pose2d DepotIntake = new Pose2d(1.086, 5.969, Rotation2d.fromRadians(3.142));
-        public static final Pose2d Hub = new Pose2d(4.62, 4.035, Rotation2d.kZero);
+        public static final Pose2d Hub = new Pose2d(4.625, 4.035, Rotation2d.kZero);
         public static final Pose2d LeftBumpStartingPosition = new Pose2d(3.582, 6.036, Rotation2d.fromRadians(3.142));
         public static final Pose2d LeftMiddleNZ = new Pose2d(7.775, 5.598, Rotation2d.fromRadians(-1.833));
         public static final Pose2d LeftNZ = new Pose2d(7.775, 7.274, Rotation2d.fromRadians(-1.833));
         public static final Pose2d LeftNZLoopEnd = new Pose2d(5.801, 5.82, Rotation2d.fromRadians(1.571));
+        public static final Pose2d LeftTrench = new Pose2d(4.625, 7.43, Rotation2d.kZero);
         public static final Pose2d LeftTrenchStartingPosition = new Pose2d(4.476, 7.596, Rotation2d.fromRadians(3.142));
         public static final Pose2d NZDepotShootingTarget = new Pose2d(2, 6.043, Rotation2d.kZero);
         public static final Pose2d NZOutpostShootingTarget = new Pose2d(2, 1.957, Rotation2d.kZero);
@@ -43,6 +44,7 @@ public final class ChoreoVars {
         public static final Pose2d RightMiddleNZ = new Pose2d(7.775, 2.471, Rotation2d.fromRadians(1.833));
         public static final Pose2d RightNZ = new Pose2d(7.775, 0.795, Rotation2d.fromRadians(1.833));
         public static final Pose2d RightNZLoopEnd = new Pose2d(5.801, 2.249, Rotation2d.fromRadians(-1.571));
+        public static final Pose2d RightTrench = new Pose2d(4.625, 0.639, Rotation2d.kZero);
         public static final Pose2d RightTrenchStartingPosition = new Pose2d(4.476, 0.473, Rotation2d.fromRadians(3.142));
         public static final Pose2d ShootLeftTrench = new Pose2d(3.08, 7.502, Rotation2d.kZero);
         public static final Pose2d ShootRightTrench = new Pose2d(3.08, 0.567, Rotation2d.kZero);
