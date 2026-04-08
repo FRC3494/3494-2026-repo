68fb73842ea7812322b7460ee82d2f3ca1fdd272
liujip0 <liujip2020@gmail.com>
2026-04-07 22:02:52 -0400

    start on drive through trench command

--- src/main/java/frc/robot/subsystems/drive/autoalign/AutoAlignCommands.java
+++ src/main/java/frc/robot/subsystems/drive/autoalign/AutoAlignCommands.java
@@ -3,10 +3,14 @@ package frc.robot.subsystems.drive.autoalign;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 import static frc.robot.Constants.DriveConstants.AutoAlignConstants.*;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.OI.DriveOI;
 import frc.robot.RobotCommands;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.DriveCommands;
 import frc.robot.util.QuadranglesUtil;
 
 public class AutoAlignCommands {
@@ -27,6 +31,7 @@ public class AutoAlignCommands {
         () -> closerToOutpostClimb(drive.getPose().getTranslation()));
   }
 
+  // TODO: make generic closerTo and closerToBlueTranslations in QuadranglesUtil
   private static boolean closerToOutpostClimb(Translation2d robotLocation) {
     double distanceToOutpostPose =
         robotLocation.getDistance(
@@ -36,5 +41,21 @@ public class AutoAlignCommands {
             QuadranglesUtil.toAllianceTranslation(climbSetupPoseDepot.getTranslation()));
 
     return distanceToOutpostPose < distanceToDepotPose;
+  }
+
+  public static Command driveThroughTrench(Drive drive) {
+    return DriveCommands.joystickDriveAtAngle(
+        drive,
+        DriveOI::joystickDriveX,
+        DriveOI::joystickDriveY,
+        () -> closestTrenchOrientation(drive.getRotation()));
+  }
+
+  private static Rotation2d closestTrenchOrientation(Rotation2d robotYaw) {
+    if (Math.abs(MathUtil.angleModulus(robotYaw.getRadians())) < Math.PI / 4.0) {
+      return Rotation2d.kZero;
+    } else {
+      return Rotation2d.k180deg;
+    }
   }
 }
