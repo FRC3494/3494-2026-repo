6e66e7b761d6a70d99920aac54be64b312cf0083
liujip0 <liujip2020@gmail.com>
2026-04-17 00:22:53 -0400

    slow down R-nz to nz second pass

--- src/main/java/frc/robot/util/choreo/ChoreoTraj.java
+++ src/main/java/frc/robot/util/choreo/ChoreoTraj.java
@@ -174,14 +174,14 @@ public record ChoreoTraj(
 	public static final ChoreoTraj RightShootToNZ_BLUE = new ChoreoTraj(
 	    "RightShootToNZ_BLUE",
 	    OptionalInt.empty(),
-	    3.48372,
+	    4.13386,
 	    new Pose2d(3.08, 0.69, Rotation2d.fromRadians(0)),
 	    new Pose2d(6.055, 5.638, Rotation2d.fromRadians(1.571))
 	);
 	public static final ChoreoTraj RightShootToNZ_RED = new ChoreoTraj(
 	    "RightShootToNZ_RED",
 	    OptionalInt.empty(),
-	    3.48372,
+	    4.13386,
 	    new Pose2d(3.08, 0.69, Rotation2d.fromRadians(0)),
 	    new Pose2d(6.055, 5.638, Rotation2d.fromRadians(1.571))
 	);
