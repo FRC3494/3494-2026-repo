3fead99cda0e64f622563506a431255e8f1a3f12
liujip0 <liujip2020@gmail.com>
2026-04-25 02:47:18 -0400

    initial hub-Lclimb auto

---
+++ src/main/java/frc/robot/autos/LeftHubToClimbAuto.java
@@ -0,0 +1,59 @@
+package frc.robot.autos;
+
+import static edu.wpi.first.wpilibj2.command.Commands.parallel;
+import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.RobotCommands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.shooter.ShooterAimModel;
+import frc.robot.util.choreo.ChoreoTraj;
+import frc.robot.util.choreo.ChoreoVars;
+
+public class LeftHubToClimbAuto extends AutoBase {
+  @Override
+  public String getName() {
+    return "LeftHubToClimb";
+  }
+
+  @Override
+  public Pose2d getStartingPose() {
+    return ChoreoVars.Poses.LeftHubStartingPosition;
+  }
+
+  @Override
+  public AutoRoutine getRoutine(
+      String routineName,
+      Alliance alliance,
+      AutoFactory autoFactory,
+      RobotCommands robotCommands,
+      Drive drive,
+      ShooterAimModel shooterAimModel) {
+    AutoRoutine routine = autoFactory.newRoutine(routineName);
+
+    AutoTrajectory leftHubToClimb = ChoreoTraj.LeftHubToClimb.asAutoTraj(routine);
+
+    routine
+        .active()
+        .onTrue(
+            sequence(
+                leftHubToClimb.resetOdometry(),
+                parallel(
+                    robotCommands.enableAutoShooterSettings(),
+                    robotCommands.enableAutoTurret(),
+                    leftHubToClimb.cmd())));
+
+    leftHubToClimb
+        .done()
+        .onTrue(
+            sequence(
+                robotCommands.shoot().withTimeout(5),
+                Autos.climbDepot(robotCommands, drive, shooterAimModel)));
+
+    return routine;
+  }
+}
